b8be353dd06875c48cf1caaf3605407a868f2b30
Allex <1allex.moura@gmail.com>
2025-09-13 11:32:22 -0300

    Validate self or admin

---
+++ src/main/java/com/devsuperior/dscommerce/services/AuthService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserService service;
+
+    public void validateSelfOrAdmin(long userId){
+
+        User user = service.authenticated();
+        if(!user.hasRole("ROLE_ADMIN") && !user.getId().equals(userId)){
+            throw new ForbiddenException("Access denied");
+        }
+
+    }
+
+}
